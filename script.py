9ac07a6a7af4fb163527d4dc667dd65708f9598a
helgebjorlow <57293023+helgebjorlow@users.noreply.github.com>
2020-06-23 20:01:30 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from pandas.io.json import json_normalize
 import json
 import os
 
@@ -30,17 +29,6 @@ def flatten_json(nested_json):
     flatten(nested_json)
     return out
 
-basepath = os.getcwd() + '/Blue Energy/18/'
-keylist = []
-with open(basepath + os.listdir(basepath)[0]) as f:
-    sample = json.load(f)
-sample = flatten_json(sample)
-for key in sample.keys():
-    keylist.append(key)
-print(keylist[6])
-
-
-
 # this finds our json files
 path_to_json = os.getcwd() + '/Blue Energy/18/'
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
@@ -50,21 +38,15 @@ jsons_data = pd.DataFrame(columns=keylist)
 jsons_data.to_csv('out.csv', encoding='utf-8', index=False)
 dataframe = ''
 # we need both the json and an index number so use enumerate()
+recordList = []
 for index, js in enumerate(json_files):
     with open(os.path.join(path_to_json, js)) as json_file:
         json_text = flatten_json(json.load(json_file))
-        # here you need to know the layout of your json and each json has to have
-        # the same structure 
-        # here I push a list of data into a pandas DataFrame at row given by 'index'
-        for attr_num in range(len(json_text)):
-            if keylist[attr_num] in json_text:
-                jsons_data.loc[index] = json_text[keylist[index]]
+        recordList.append(json_text)
+print(len(recordList))
 
-jsons_data.to_csv('out.csv', encoding='utf-8', index=False)
-# now that we have the pertinent json data in our DataFrame let's look at it
-
-
-
+df = pd.DataFrame(recordList)
+export = df.to_csv('out.csv')
 
 
 
